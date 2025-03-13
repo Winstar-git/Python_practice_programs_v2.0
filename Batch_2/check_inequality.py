3612a812839eb5549d7fa43f740ecdf7cd39d149
Winstar <james.canoy87@gmail.com>
2025-03-13 20:17:05 +0800

    Compare two numbers and display if they are equal or not

--- Batch_2/check_inequality.py
+++ Batch_2/check_inequality.py
@@ -2,7 +2,13 @@
 # Print "Not Equal" when the numbers are not the same.
 
 # Ask the user to input the first number.
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number.
+num2 = int(input("Enter the second number: "))
 # Compare the two numbers.
+if num1 != num2:
 # If the numbers are not the same, print "Not Equal".
-# If the numbers are not the same, print "Equal".
+    print(f"{num1} and {num2} are Not Equal")
+# If the numbers are the same, print "Equal".
+else:
+    print(f"{num1} and {num2} are Equal")
