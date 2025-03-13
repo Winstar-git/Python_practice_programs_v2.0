2d681d78abecf3742ab12ac25db06a481160ec3f
Winstar <james.canoy87@gmail.com>
2025-03-13 18:43:20 +0800

    Pseudocode for batch1/program3

--- Batch_1/sum_of_two_numbers.py
+++ Batch_1/sum_of_two_numbers.py
@@ -1,2 +1,6 @@
 # Prog03: Create a program that ask user to input 2 numbers.
 # Print the sum of the two numbers.
+
+# Ask the user to input the first number.
+# Ask the user to input the second number.
+# Calculate the sum of the two numbers amd print the sum..
