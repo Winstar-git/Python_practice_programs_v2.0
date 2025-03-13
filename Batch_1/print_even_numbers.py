8db11be9f48e39ed81abb1147b735e96f532d9d1
Winstar <james.canoy87@gmail.com>
2025-03-13 19:37:09 +0800

    Syntax Error-forgot to add colon symbol

--- Batch_1/print_even_numbers.py
+++ Batch_1/print_even_numbers.py
@@ -1,2 +1,8 @@
 #Prog09: Create a program that print all the even 
 #numbers starting from 0 to 100. (Use for loop)
+
+# Loop from 0 to 100.
+# Set the range from 0, stop 101, and the step to 2 for even numbers.
+for i in range(0, 101, 2)
+#   Print the result
+    print(i, end="-")
