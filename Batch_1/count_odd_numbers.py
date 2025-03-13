32296eec03905f049d1a2b83f47da23af8651588
Winstar <james.canoy87@gmail.com>
2025-03-13 19:23:12 +0800

    Pseudocode

--- Batch_1/count_odd_numbers.py
+++ Batch_1/count_odd_numbers.py
@@ -1,2 +1,8 @@
 #Prog08: Create a program that ask user to input 10 numbers. 
 #Print how many are odd numbers.
+
+# Initialize a counter for odd numbers.
+# Loop to ask the user to input 10 numbers.
+#     Check if each number is odd.
+#     Increment the counter if the number is odd.
+# Print the counter.
