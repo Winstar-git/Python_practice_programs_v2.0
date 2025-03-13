2e855a5e76fb049e42b89bed9ada24e45c66d438
Winstar <james.canoy87@gmail.com>
2025-03-13 21:55:24 +0800

    Initialize list and implement loop for user number input

--- Batch_3/non_duplicate_numbers_finder.py
+++ Batch_3/non_duplicate_numbers_finder.py
@@ -2,9 +2,13 @@
 # Display all numbers that don't have duplicate.
 
 # Initialize an empty list to store the numbers.
+numbers = []
 # Loop to ask the user to input 10 numbers.
+for i in range(10):
+    num = int(input(f"{i+1}. Enter a number: "))
 #     Append each number to the list.
-# Initialize an empty list to store unique numbers.
+    numbers.append(num)
+# Initialize an empty list to store unique numbers
 # Loop through the list of numbers.
 #     If the number appears only once in the list, add it to the unique numbers list.
 # Display the unique numbers.
