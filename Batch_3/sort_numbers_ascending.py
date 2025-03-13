cddb4001fb4ed364de10588bb769b63b86c1316f
Winstar <james.canoy87@gmail.com>
2025-03-13 23:51:34 +0800

    Implement sorting of user-inputted numbers from lowest to highest

--- Batch_3/sort_numbers_ascending.py
+++ Batch_3/sort_numbers_ascending.py
@@ -3,9 +3,19 @@
 # Display the number from lowest to highest. Clue: sort() function.
 
 # Initialize an empty list to store the numbers.
+numbers = []
 # Loop to ask the user to input numbers until invalid input.
-#     Try to convert the input to a number.
-#         If successful, add the number to the list.
+while True:
+    try:
+#       Try to convert the input to a number.
+        num = int(input("Enter a number: "))
+#        If successful, add the number to the list.
+        numbers.append(num)
 #     If input is invalid, break the loop.
+    except ValueError:
+        print("Invalid Input. Exitinng the program")
+        break
 # Sort the list of numbers.
+result = sorted(numbers)
 # Display the sorted list of numbers.
+print(f"Lowest to Highest: {result}")
