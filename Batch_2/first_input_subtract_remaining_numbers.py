0c4f997456e23aa19c3d3c1d1bc343d7c4444a91
Winstar <james.canoy87@gmail.com>
2025-03-13 21:01:59 +0800

    Calculate the difference between first number and sum of others

--- Batch_2/first_input_subtract_remaining_numbers.py
+++ Batch_2/first_input_subtract_remaining_numbers.py
@@ -1,8 +1,13 @@
 #Prog06: Create a program that ask user to input 10 numbers. 
 # Print the result of the first number minus all of the remaining numbers.
 
-# Pseudo code:
-# Initialize a variable to store the result.
+# Initialize a variable to store the input.
+numbers = []
 # Loop to ask the user to input 10 numbers.
-#     Subtract the first number to all of the remaining numbers..
+for i in range(10):
+    num = int(input(f"{i+1}. Enter a number: "))
+    numbers.append(num)
+#     Subtract the first number to all of the remaining numbers.
+    result = numbers[0] - sum(numbers[1:])
 # Print the result.
+print(result)
