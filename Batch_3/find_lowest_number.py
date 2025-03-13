29c876d444c8ce186ddfe55eaa14f5886d8ff1da
Winstar <james.canoy87@gmail.com>
2025-03-13 23:42:03 +0800

    Store user inputs and determine the minimum value

--- Batch_3/find_lowest_number.py
+++ Batch_3/find_lowest_number.py
@@ -2,8 +2,17 @@
 # continue asking until the user input is invalid. Display the lowest number.
 
 # Initialize an empty list to store the numbers.
+numbers = []
 # Loop to ask the user to input numbers until invalid input.
-#     Try to convert the input to a number.
-#         If successful, add the number to the list.
-#     If input is invalid, break the loop.
+while True:
+    try:
+#       Try to convert the input to a number.
+        num = int(input("Enter a number: "))
+#       If successful, add the number to the list.
+        numbers.append(num)
+#   If input is invalid, break the loop.
+    except ValueError:
+        print("Invalid Input. Exitinng the program")
+        break
 # Find and display the lowest number in the list.
+print(f"The lowest numbers is: {min(numbers)}")
