8964c6854db8e3d243dd9e610b1655c76ef46637
Winstar <james.canoy87@gmail.com>
2025-03-13 19:47:16 +0800

    Pseudocode to print numbers ending without zero

--- Batch_1/skip_numbers_ending_with_zero.py
+++ Batch_1/skip_numbers_ending_with_zero.py
@@ -1,3 +1,6 @@
 #Prog10: Create a program that print all the 
 #numbers starting from 0 to 100 except numbers ending in zero.
 
+# Loop from 0 to 100.
+#     Check if the current number does not end in zero.
+#     If the number does not end in zero, print it.
