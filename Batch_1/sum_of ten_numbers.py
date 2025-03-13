1993c1ac44692d55c68d8679d6b216812815c91a
Winstar <james.canoy87@gmail.com>
2025-03-13 19:19:19 +0800

    Using for loop for 10 inputs and increment each input

--- Batch_1/sum_of ten_numbers.py
+++ Batch_1/sum_of ten_numbers.py
@@ -2,6 +2,11 @@
 #Print the sum of all the numbers.
 
 # Initialize a variable to store the sum.
+sum = 0
 # Loop to ask the user to input 10 numbers.
-#     Add each number to the sum.
+for i in range(10):
+#    Add each number to the sum.
+    num = int(input(f"{i+1}. Enter a number: "))
+    sum += num
 # Print the sum.
+print(f"The sum of all the numbers is: {sum}")
