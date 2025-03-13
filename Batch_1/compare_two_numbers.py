8be69a6c43bd98355bde6a0f8c8c214728b7fd46
Winstar <james.canoy87@gmail.com>
2025-03-13 17:35:56 +0800

    Pseudocode for batch1/program1

---
+++ Batch_1/compare_two_numbers.py
@@ -0,0 +1,8 @@
+# Prog01: Create a program that ask user to input 2 numbers.
+# Print the bigger number.
+
+# Ask the user to input the first number
+# Ask the user to input the second number
+# Compare the two numbers
+    # If the first number is bigger, print it
+    # If the second number is bigger, print it
