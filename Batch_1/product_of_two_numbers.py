78ac95cfc325174ad9695c7894b6c0d870b7b114
Winstar <james.canoy87@gmail.com>
2025-03-13 18:54:14 +0800

    Pseudocode for batch1/program4

--- Batch_1/product_of_two_numbers.py
+++ Batch_1/product_of_two_numbers.py
@@ -1,2 +1,6 @@
 #Prog04: Create a program that ask user to input 2 numbers. 
 #Print the product of the two numbers.
+
+# Ask the user to input the first number.
+# Ask the user to input the second number.
+# Calculate the product of the two numbers and print the product.
