74f38351c09c1f5d05e0c528b6a20587fa32ea73
Winstar <james.canoy87@gmail.com>
2025-03-13 20:19:07 +0800

    Pseudocode to calculate the difference of two numbers

--- Batch_2/p3.py
+++ Batch_2/p3.py
@@ -1,2 +1,7 @@
 #Prog03: Create a program that ask user to input 2 numbers. 
 # Print the difference of the two numbers.
+
+# Ask the user to input the first number.
+# Ask the user to input the second number.
+# Calculate the difference between the two numbers.
+# Print the difference.
