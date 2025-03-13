b03852fc568fc717754d8fc655160567ceb0323d
Winstar <james.canoy87@gmail.com>
2025-03-13 23:28:17 +0800

    Fix error in pseudocode logic for number uniqueness check

--- Batch_3/unique_number_checker.py
+++ Batch_3/unique_number_checker.py
@@ -4,8 +4,21 @@
 # Display "Duplicate" after input when the inputted number have duplicate.
 
 # Initialize an empty list to store the numbers.
+numbers = []
 # Loop to ask the user to input numbers until invalid input.
-#     Try to convert the input to a number.
-#         If successful, add the number to the list.
-#         Check if the number is unique or duplicate and display the result.
-#     If input is invalid, break the loop.
+while True:
+    try:
+        num =input("Enter a number: ")
+#       Try to convert the input to a number.
+        num = int(num)
+#       Check if the number is unique or duplicate and display the result.
+        if num in numbers:
+            print("Duplicate")
+        else:
+            print("Unique")
+#           Add to list for new input numbers
+            numbers.append(num)            
+#    If input is invalid, break the loop.
+    except ValueError:
+        print("Invalid Input. Exitinng the program")
+        break
