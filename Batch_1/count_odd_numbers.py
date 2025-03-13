aa7bef78d6ec11365cbaf28a32c98691244644ea
Winstar <james.canoy87@gmail.com>
2025-03-13 19:28:41 +0800

    Count and display the number of odd values

--- Batch_1/count_odd_numbers.py
+++ Batch_1/count_odd_numbers.py
@@ -2,7 +2,13 @@
 #Print how many are odd numbers.
 
 # Initialize a counter for odd numbers.
+count = 0 
 # Loop to ask the user to input 10 numbers.
-#     Check if each number is odd.
+for i in range(10):
+    num = int(input(f"{i+1}. Enter a number: "))
+#   Check if each number is odd.
+    if num % 2 != 0:
 #     Increment the counter if the number is odd.
+         count += 1
 # Print the counter.
+print(f"There are {count} odd numbers.")
