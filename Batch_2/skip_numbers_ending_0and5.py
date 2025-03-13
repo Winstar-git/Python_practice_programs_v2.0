0ae4037aef7df6f28abd49e56b423568a6561d6c
Winstar <james.canoy87@gmail.com>
2025-03-13 21:30:14 +0800

    Filter and print numbers not divisible by 5 from 0 to 100

--- Batch_2/skip_numbers_ending_0and5.py
+++ Batch_2/skip_numbers_ending_0and5.py
@@ -2,5 +2,8 @@
 # starting from 0 to 100 except numbers ending in zero or ending five.
 
 # Loop from 0 to 100
-    # Check if the number is NOT a multiple of 10
+for i in range(0, 101):
+    # Check if the number is NOT a multiple of 5
+    if i % 5 != 0:
         # Print the number on the same line
+        print(i, end="-")
