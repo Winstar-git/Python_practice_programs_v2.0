576ff1e50e3bd1112da70d2a3caf1a928b3e38ce
Winstar <james.canoy87@gmail.com>
2025-03-14 00:39:59 +0800

    Implement loop to accept numbers and find the highest value

--- Batch_4/display_highest_number.py
+++ Batch_4/display_highest_number.py
@@ -3,7 +3,17 @@
 # Display the highest number.
 
 # Initialize an empty list to store the numbers.
+numbers = []
 # Loop to ask the user to input numbers until invalid input.
+while True:
+    try:
+        num = int(input("Enter a number: "))
 #     If successful, add the number to the list.
+        numbers.append(num)
 #     If input is invalid, break the loop.
+    except ValueError:
+        print("Invalid Input. Exitinng the program")
+        break
 # Find and display the highest number in the list.
+highest_number = max(numbers)
+print(f"The highest number is: {highest_number}")
