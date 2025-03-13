f9e59fdccaf11585264390dd06a50fd900a481ee
Winstar <james.canoy87@gmail.com>
2025-03-13 20:32:05 +0800

    Calculate remainder when dividing two numbers

--- Batch_2/find_the_remainder.py
+++ Batch_2/find_the_remainder.py
@@ -2,6 +2,9 @@
 # Print the remainder when the first number is divided by the second number.
 
 # Ask the user to input the first number.
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number.
+num2 = int(input("Enter the second number: "))
 # Calculate the remainder when the first number is divided by the second number.
 # Print the remainder.
+print(f"The remainder of the two numbers is {num1%num2}")
