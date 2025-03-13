47c2a15c089750eefdbb47e4b05f355238a4c417
Winstar <james.canoy87@gmail.com>
2025-03-14 00:58:06 +0800

    Implement average calculation for user-inputted numbers

--- Batch_4/compute_the_average.py
+++ Batch_4/compute_the_average.py
@@ -3,9 +3,18 @@
 # Display the average.
 
 # Initialize an empty list to store the numbers.
+numbers = []
 # Loop to ask the user to input numbers until invalid input.
-#     Try to convert the input to a number.
-#         If successful, add the number to the list.
+while True:
+    try:
+        num = int(input("Enter a number: "))
+#     If successful, add the number to the list.
+        numbers.append(num)
 #     If input is invalid, break the loop.
+    except ValueError:
+        print("Invalid Input. Exitinng the program")
+        break
 # Calculate the average of the numbers in the list.
+average = sum(numbers)/(len(numbers))
 # Display the average.
+print(f"The average number is: {average:.2f}")
