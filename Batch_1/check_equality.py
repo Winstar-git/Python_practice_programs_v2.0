ba67b3873e04cd639043501179bc2ea2dda2643d
Winstar <james.canoy87@gmail.com>
2025-03-13 18:08:27 +0800

    Check if two numbers are equal or not

--- Batch_1/check_equality.py
+++ Batch_1/check_equality.py
@@ -2,7 +2,13 @@
 # Print "Equal" when the numbers are the same.
 
 # Ask the user to input the first number
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number
+num2 = int(input("Enter the second number: "))
 # Compare the two numbers
+if num1 == num2:
 # Print "Equal" if the numbers are the same
+    print(f"{num1} and {num2} are Equal")
 # Print "Not Equal" if the numbers are not the same
+else:
+    print(f"{num1} and {num2} are Not Equal")
