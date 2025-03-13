12891482851bae9ef21289a16b6805d2761b57d3
Winstar <james.canoy87@gmail.com>
2025-03-13 17:57:52 +0800

    Pseudocode for batch1/program2

--- Batch_1/check_equality.py
+++ Batch_1/check_equality.py
@@ -1,2 +1,8 @@
 # Prog02: Create a program that ask user to input 2 numbers.
 # Print "Equal" when the numbers are the same.
+
+# Ask the user to input the first number
+# Ask the user to input the second number
+# Compare the two numbers
+# Print "Equal" if the numbers are the same
+# Print "Not Equal" if the numbers are not the same
