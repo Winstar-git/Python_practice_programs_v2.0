e27a1a4bf8b4deae513924539643946378278ac1
Winstar <james.canoy87@gmail.com>
2025-03-13 20:35:56 +0800

    Pseudocode to subract the first number to the remaining number

---
+++ Batch_2/first_input_subtract_remaining_numbers.py
@@ -0,0 +1,8 @@
+#Prog06: Create a program that ask user to input 10 numbers. 
+# Print the result of the first number minus all of the remaining numbers.
+
+# Pseudo code:
+# Initialize a variable to store the result.
+# Loop to ask the user to input 10 numbers.
+#     Subtract the first number to all of the remaining numbers..
+# Print the result.
