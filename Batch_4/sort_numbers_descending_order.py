96f8944e02db7f0b9b0725d64455f6dbe75bfba5
Winstar <james.canoy87@gmail.com>
2025-03-14 00:48:44 +0800

    Implement sorting to display numbers from highest to lowest

--- Batch_4/sort_numbers_descending_order.py
+++ Batch_4/sort_numbers_descending_order.py
@@ -3,9 +3,18 @@
 # Display the number from highest to lowest. Clue: sort() function.
 # 
 # Initialize an empty list to store the numbers.
+numbers = []
 # Loop to ask the user to input numbers until invalid input.
-#     Try to convert the input to a number.
-#         If successful, add the number to the list.
+while True:
+    try:
+        num = int(input("Enter a number: "))
+#     If successful, add the number to the list.
+        numbers.append(num)
 #     If input is invalid, break the loop.
+    except ValueError:
+        print("Invalid Input. Exitinng the program")
+        break
 # Sort the list of numbers in descending order.
+descending_order = sorted(numbers, reverse=True)
 # Display the sorted list of numbers.
+print(f"Highest to Lowest: {descending_order}")
