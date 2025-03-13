e968437e2485110872c98171244d4ee026c0b2e5
Winstar <james.canoy87@gmail.com>
2025-03-14 00:51:09 +0800

    Pseudocode to display to calculate the average number

---
+++ Batch_4/compute_the_average.py
@@ -0,0 +1,11 @@
+#Prog05: Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the average.
+
+# Initialize an empty list to store the numbers.
+# Loop to ask the user to input numbers until invalid input.
+#     Try to convert the input to a number.
+#         If successful, add the number to the list.
+#     If input is invalid, break the loop.
+# Calculate the average of the numbers in the list.
+# Display the average.
