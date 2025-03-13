d59502823119272bd8926e25ef5a9b8f898fbfd2
Winstar <james.canoy87@gmail.com>
2025-03-13 21:19:35 +0800

    Print all odd numbers from 0 to 100 using while loop

---
+++ Batch_2/print_odd_numbers.py
@@ -0,0 +1,13 @@
+#Prog08: Create a program that print all the odd numbers 
+# starting from 0 to 100. (Use while loop)
+
+# Initialize a variable to 0.
+num = 1
+# Loop while the variable is less than or equal to 100.
+while num <= 100:
+#     Check if the variable is odd.
+    if num % 2 != 0:
+#     If the variable is odd, print it.
+        print(num, end="-")
+#     Increment the variable by 1.
+    num += 1
