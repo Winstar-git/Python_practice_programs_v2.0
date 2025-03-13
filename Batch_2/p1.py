a1aa7ee4ded29219e80b28c5ca0abd497b48bd7d
Winstar <james.canoy87@gmail.com>
2025-03-13 20:05:15 +0800

    Pseudocode to print tha small number

--- Batch_2/p1.py
+++ Batch_2/p1.py
@@ -1,2 +1,6 @@
 # Prog01: Create a program that ask user to input 2 numbers. 
 # Print the smaller number.
+
+# Ask the user to input the first number.
+# Ask the user to input the second number.
+# Compare the two numbers. Print the smaller number.
