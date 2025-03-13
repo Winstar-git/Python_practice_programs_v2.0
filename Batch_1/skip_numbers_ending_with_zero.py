7de2f1ad40ad45aac18ffdfb381dbf314867061e
Winstar <james.canoy87@gmail.com>
2025-03-13 19:50:10 +0800

    Add loop to print numbers from 1 to 100, skipping multiples of 10

--- Batch_1/skip_numbers_ending_with_zero.py
+++ Batch_1/skip_numbers_ending_with_zero.py
@@ -2,5 +2,8 @@
 #numbers starting from 0 to 100 except numbers ending in zero.
 
 # Loop from 0 to 100.
+for i in range(1, 101):
 #     Check if the current number does not end in zero.
+    if i % 10 !=0:
 #     If the number does not end in zero, print it.
+        print(i, end="-")
