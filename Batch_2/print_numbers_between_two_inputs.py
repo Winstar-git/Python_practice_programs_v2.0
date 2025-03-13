6684c8c48ce1bee2c847912434e5f7a7f923a99e
Winstar <james.canoy87@gmail.com>
2025-03-13 21:42:28 +0800

    Print numbers between two inputs using for loop

--- Batch_2/print_numbers_between_two_inputs.py
+++ Batch_2/print_numbers_between_two_inputs.py
@@ -2,6 +2,14 @@
 # numbers. Print all the numbers between the two numbers.
 
 # Ask the user to input the first number.
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number.
+num2 = int(input("Enter the second number: "))
 #Loop from the smaller number to the larger number.
+if num1 < num2:
+    for i in range(num1 + 1,num2):
 #     Print each number in the range.
+        print(i, end="-")
+else:
+    for i in range(num2 + 1, num1):
+            print(i, end="-")
