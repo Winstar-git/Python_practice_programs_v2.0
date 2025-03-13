bfbe0b2bd41eb49f75992a553442f29cf9eba3fd
Winstar <james.canoy87@gmail.com>
2025-03-13 20:22:28 +0800

    Calculate the difference of two numbers

--- Batch_2/difference_of two_numbers.py
+++ Batch_2/difference_of two_numbers.py
@@ -2,6 +2,9 @@
 # Print the difference of the two numbers.
 
 # Ask the user to input the first number.
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number.
+num2 = int(input("Enter the second number: "))
 # Calculate the difference between the two numbers.
 # Print the difference.
+print(f"The difference of {num1} and {num2} is {num1 - num2}")
