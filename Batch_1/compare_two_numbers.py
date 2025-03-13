e46117142bbdac7ac6fc656e4a6b371198666228
Winstar <james.canoy87@gmail.com>
2025-03-13 17:43:39 +0800

    Display which number is bigger

--- Batch_1/compare_two_numbers.py
+++ Batch_1/compare_two_numbers.py
@@ -6,5 +6,9 @@ num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number
 num2 = int(input("Enter the second number: "))
 # Compare the two numbers
+if num1 > num2:
     # If the first number is bigger, print it
+    print(f"{num1} is bigger number")
+else:
     # If the second number is bigger, print it
+    print(f"{num2} is bigger number")
