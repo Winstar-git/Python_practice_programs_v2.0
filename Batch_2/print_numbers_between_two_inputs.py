567e08178c4f1877453f2fd6d02e18c4ecab0868
Winstar <james.canoy87@gmail.com>
2025-03-13 21:37:09 +0800

    Pseudocode to print numbers between two inputs using for loop

---
+++ Batch_2/print_numbers_between_two_inputs.py
@@ -0,0 +1,7 @@
+#Prog10: Create a program that ask user to input 2 
+# numbers. Print all the numbers between the two numbers.
+
+# Ask the user to input the first number.
+# Ask the user to input the second number.
+#Loop from the smaller number to the larger number.
+#     Print each number in the range.
