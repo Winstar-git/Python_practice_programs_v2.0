d7e47099765a55b7f8323c3b12c72e1f97ea3e0c
Winstar <james.canoy87@gmail.com>
2025-03-13 23:45:09 +0800

    Pseudocode to sort numbers from lowest to highest

---
+++ Batch_3/sort_numbers_ascending.py
@@ -0,0 +1,11 @@
+# Prog05: Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the number from lowest to highest. Clue: sort() function.
+
+# Initialize an empty list to store the numbers.
+# Loop to ask the user to input numbers until invalid input.
+#     Try to convert the input to a number.
+#         If successful, add the number to the list.
+#     If input is invalid, break the loop.
+# Sort the list of numbers.
+# Display the sorted list of numbers.
