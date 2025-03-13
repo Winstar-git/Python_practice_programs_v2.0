647f1ca8453bed6723c4eec6f26e13d63c69a373
Winstar <james.canoy87@gmail.com>
2025-03-13 19:09:48 +0800

    Add exponentiation operation for two numbers

--- Batch_1/exponentiation.py
+++ Batch_1/exponentiation.py
@@ -2,5 +2,8 @@
 # Print the result when the first number is raised to the second number.
 
 # Ask the user to input the first number.
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number.
+num2 = int(input("Enter the second number: "))
 # Calculate the result of the first number raised to the second number.Print the result.
+print(f"{num1} raised to the power of {num2} is {num1**num2}")
