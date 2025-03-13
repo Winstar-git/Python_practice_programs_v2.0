54915d51bf808a05d782da60d734e9df1fe53d6b
Winstar <james.canoy87@gmail.com>
2025-03-13 20:25:57 +0800

    Pseudocode to calculate the quotient without decimal point

---
+++ Batch_2/quotient_without_decimal.py
@@ -0,0 +1,7 @@
+#Prog04: Create a program that ask user to input 2 numbers. 
+# Print the quotient of the two numbers without the decimal point.
+
+# Ask the user to input the first number.
+# Ask the user to input the second number.
+# Calculate the quotient of the two numbers.
+# Print the quotient without the decimal point.
