efe7dca1fd4b5ca2ab7e406ee7382d00e5b9279d
Winstar <james.canoy87@gmail.com>
2025-03-13 20:13:39 +0800

    Pseudocode to check inequality

---
+++ Batch_2/check_inequality.py
@@ -0,0 +1,8 @@
+#Prog02: Create a program that ask user to input 2 numbers. 
+# Print "Not Equal" when the numbers are not the same.
+
+# Ask the user to input the first number.
+# Ask the user to input the second number.
+# Compare the two numbers.
+# If the numbers are not the same, print "Not Equal".
+# If the numbers are not the same, print "Equal".
--- Batch_2/p2.py
+++
@@ -1,2 +0,0 @@
-#Prog02: Create a program that ask user to input 2 numbers. 
-# Print "Not Equal" when the numbers are not the same.
