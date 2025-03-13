7d9589a326f8d6ec5079642ab2a7676eff077aeb
Winstar <james.canoy87@gmail.com>
2025-03-13 19:37:51 +0800

    Fix syntax error

--- Batch_1/print_even_numbers.py
+++ Batch_1/print_even_numbers.py
@@ -3,6 +3,6 @@
 
 # Loop from 0 to 100.
 # Set the range from 0, stop 101, and the step to 2 for even numbers.
-for i in range(0, 101, 2)
+for i in range(0, 101, 2):
 #   Print the result
     print(i, end="-")
