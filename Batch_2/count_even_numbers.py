d4cc7d90713d835ac06d0ddd9607d16914957b0e
Winstar <james.canoy87@gmail.com>
2025-03-13 21:08:14 +0800

    Pseudocode to count how many even numbers

---
+++ Batch_2/count_even_numbers.py
@@ -0,0 +1,8 @@
+#Prog07: Create a program that ask user to input 10 numbers. 
+# Print how many are even numbers.
+
+# Initialize a counter for even numbers.
+# Loop to ask the user to input 10 numbers.
+#     Check if each number is even.
+#     Increment the counter if the number is even.
+# Print the counter.
