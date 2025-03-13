1227ef76fbbcc76129cd9fd54df86f1148bd3d6a
Winstar <james.canoy87@gmail.com>
2025-03-14 00:34:35 +0800

    Pseudocode for finding the highest number

---
+++ Batch_4/display_highest_number.py
@@ -0,0 +1,9 @@
+#Prog03: Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the highest number.
+
+# Initialize an empty list to store the numbers.
+# Loop to ask the user to input numbers until invalid input.
+#     If successful, add the number to the list.
+#     If input is invalid, break the loop.
+# Find and display the highest number in the list.
