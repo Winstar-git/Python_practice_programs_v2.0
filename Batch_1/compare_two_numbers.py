04c243b33edcff5ae4659940a5fdd3aa5a4e67b3
Winstar <james.canoy87@gmail.com>
2025-03-13 17:38:20 +0800

    Add user input for numbers

--- Batch_1/compare_two_numbers.py
+++ Batch_1/compare_two_numbers.py
@@ -2,7 +2,9 @@
 # Print the bigger number.
 
 # Ask the user to input the first number
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number
+num2 = int(input("Enter the second number: "))
 # Compare the two numbers
     # If the first number is bigger, print it
     # If the second number is bigger, print it
