7157059128731a7fead1921b77e38fcf0e675ca0
Winstar <james.canoy87@gmail.com>
2025-03-13 20:08:27 +0800

    Display small value input

--- Batch_2/p1.py
+++ Batch_2/p1.py
@@ -2,5 +2,11 @@
 # Print the smaller number.
 
 # Ask the user to input the first number.
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number.
+num2 = int(input("Enter the second number: "))
+if num1 < num2:
 # Compare the two numbers. Print the smaller number.
+    print(f"{num1} is smaller number.")
+else:
+    print(f"{num2} is smaller number.")
