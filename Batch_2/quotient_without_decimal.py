702cf68ad4f37a974ce1f0f9987290e658d711d9
Winstar <james.canoy87@gmail.com>
2025-03-13 20:27:35 +0800

    Calculate the quotient of two numbers without decimal point

--- Batch_2/quotient_without_decimal.py
+++ Batch_2/quotient_without_decimal.py
@@ -2,6 +2,9 @@
 # Print the quotient of the two numbers without the decimal point.
 
 # Ask the user to input the first number.
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number.
+num2 = int(input("Enter the second number: "))
 # Calculate the quotient of the two numbers.
 # Print the quotient without the decimal point.
+print(f"The quotient of the two numbers is: {num1/num2:.0f}" )
