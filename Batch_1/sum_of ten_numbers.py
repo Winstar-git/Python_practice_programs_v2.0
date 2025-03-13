b36ad8a322c0e6525899615a5710a128a9fac758
Winstar <james.canoy87@gmail.com>
2025-03-13 19:13:57 +0800

    Pseudocode for batch1/program6

--- Batch_1/sum_of ten_numbers.py
+++ Batch_1/sum_of ten_numbers.py
@@ -1,2 +1,7 @@
 #Prog07: Create a program that ask user to input 10 numbers. 
 #Print the sum of all the numbers.
+
+# Initialize a variable to store the sum.
+# Loop to ask the user to input 10 numbers.
+#     Add each number to the sum.
+# Print the sum.
