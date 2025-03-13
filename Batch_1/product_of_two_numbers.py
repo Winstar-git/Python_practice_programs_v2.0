e62d82f12d2858dd2000c0178140fd08bb148914
Winstar <james.canoy87@gmail.com>
2025-03-13 18:56:17 +0800

    multiplying the two input numbers

--- Batch_1/product_of_two_numbers.py
+++ Batch_1/product_of_two_numbers.py
@@ -2,5 +2,8 @@
 #Print the product of the two numbers.
 
 # Ask the user to input the first number.
+num1 = int(input("Enter the first number: "))
 # Ask the user to input the second number.
+num2 = int(input("Enter the second number: "))
 # Calculate the product of the two numbers and print the product.
+print("The product of the two numbers is: ", num1*num2) 
